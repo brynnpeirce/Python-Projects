db41db7f715fb1b2e9095e603891738e042f042d
brynnpeirce <103545682+brynnpeirce@users.noreply.github.com>
2022-06-28 20:45:47 -0700

    Polymorphism submission
    1 parent class
    2 child class
    parent class has 1 method
    both child classes utilize polymorphism on the parent class method

---
+++ variables.py
@@ -0,0 +1,68 @@
+
+
+# Parent class Job
+class Job:
+    title = 'Unknown'
+    Salary = 'unknown'
+    start_date = 'Unknown'
+    benefits = True
+
+    def Resume(self):
+        name = input("Enter your name: ")
+        email = input ("Enter your email: ")
+        job_experience = input ("Enter job experience: ")
+        education = input("Enter education information: ")
+        if (name == ''):
+            print("Thanks for applying!")
+        else:
+            print("You need at least your name entered to apply.")
+
+# Child class Restaurant
+class Restaurant(Job):
+    title = 'Head Chef'
+    Salary = 50,000.00
+    start_date = '01/02/23'
+
+    def Resume(self):
+        name = input("Enter your name: ")
+        email = input ("Enter your email: ")
+        cooking_experience = input ("Enter cooking experience: ")
+        if (name == ''):
+            print("Thanks for applying!")
+        else:
+            print("You need at least your name entered to apply.") 
+
+
+# Child class Hospital
+class Hospital(Job):
+    title = 'Nurse'
+    Salary = 75,000.00
+    start_date = 'ASAP'
+
+    def Resume(self):
+        name = input("Enter your name: ")
+        email = input ("Enter your email: ")
+        Healthcare_experience = input ("Enter healthcare experience: ")
+        education = input("Enter education information: ")
+        if (name == ''):
+            print("Thanks for applying!")
+        else:
+            print("You need at least your name entered to apply.")
+
+
+
+job = Job()
+job.Resume()
+
+Applicant1 = Restaurant()
+Applicant1.Resume()
+
+Applicant2 = Hospital()
+Applicant2.Resume()
+
+
+    
+
+
+
+    
