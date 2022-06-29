a088254f094aeab9524eacaba6e6d8da5c212560
brynnpeirce <103545682+brynnpeirce@users.noreply.github.com>
2022-06-28 18:26:56 -0700

    updated database assignment
    updated!

--- database.py
+++ database.py
@@ -16,23 +16,12 @@ conn.close() #closed connection to db
     
 conn = sqlite3.connect('data.bd') #connection with db
 
-with conn: # with loop. While this db is open ....
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_data(col_datatype) VALUES (?)", \
-                ('information.docx','Hello.txt','myImage.png','myMovie.mpg', \
-                 'World.txt','data.pdf','myPhoto.jpg')) # added values to tbl_data
-    conn.commit() #saved changes
-conn.close() # closed connection
-
-
-conn = sqlite3.connect('data.db') # connection with db
-
-with conn: # with loop. While this db is open ...
-    cur = conn.cursor()
-    # selecting all datatype that have 'txt'
-    cur.execute("SELECT col_datatype FROM tbl_data WHERE col_datatype LIKE 'txt%'")
-    varTxt = cur.fetchall()
-    for item in varTxt:
-        msg = "txt documents: ()".format(item[0]) #created a variable for all items that were selected
-    print(msg) #print variable that has selected file names
-
+fileList = ('information.docx','Hello.txt','myImage.png','myMovie.mpg', \
+            'World.txt','data.pdf','myPhoto.jpg')
+for x in fileList:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_data (col_datatype) VALUES (?)",(x))
+            print(x)
+conn.close()
